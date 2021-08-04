9453156e44f9033c56e67f48f3baf2d9bac5410b
Sipondo <tt_robroek@hotmail.com>
2021-08-04 20:11:15 +0200

    ask splash

--- game/upl/upl_scripts/ask.py
+++ game/upl/upl_scripts/ask.py
@@ -10,6 +10,8 @@ class Ask:
         self.act.game.m_gst.current_state.dialogue = self.obj
         self.act.game.m_gst.current_state.author = self.user.name
         self.act.game.m_gst.current_state.options = self.options
+        if hasattr(self.user, "splash") and self.user.splash:
+            self.act.game.m_gst.current_state.spr_talker = self.user.splash
 
     def on_tick(self, time=None, frame_time=None):
         if self.act.game.m_gst.current_state.dialogue is not None:
