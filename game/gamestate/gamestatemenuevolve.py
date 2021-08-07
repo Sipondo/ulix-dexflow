02bd314b6d8234b4e3e9f0a029c69ef8ef2c4d69
Jetse <jetse1796@outlook.com>
2021-08-07 21:42:43 +0200

    evolution fixed

--- game/gamestate/gamestatemenuevolve.py
+++ game/gamestate/gamestatemenuevolve.py
@@ -11,16 +11,18 @@ class GameStateMenuEvolve(BaseGameState):
         self.evolve = False
         for member in self.game.inventory.members:
             evolution_data = member.data["evolutions"]
+            print(member.name, evolution_data)
             if evolution_data != evolution_data:
                 continue
             evolve_target, cond, cond_req = evolution_data.split(",")
             if cond == "Level":
                 if member.level > int(cond_req):
-                    self.to_evolve.append((member, self.game.m_pbs.get_figher_by_name(evolve_target)))
+                    self.to_evolve.append((member, self.game.m_pbs.get_fighter_by_name(evolve_target)))
 
         self.spr_statusbox = self.game.m_res.get_interface("statusbox")
 
         self.evo = None
+        self.evo_name = None
         self.evo_target = None
 
         self.need_to_redraw = False
@@ -29,20 +31,24 @@ class GameStateMenuEvolve(BaseGameState):
         self.small_splash = None
         self.big_splash = None
 
+        self.get_evolutions()
+
     def on_tick(self, time, frame_time):
+        self.time = time
+        self.redraw(time, frame_time)
+        return False
+
+    def get_evolutions(self):
         if not self.evolving:
             if len(self.to_evolve) > 0:
                 self.evo, self.evo_target = self.to_evolve.pop()
+                self.evo_name = self.evo.name
                 self.get_evo_data()
                 self.evolving = True
                 self.evolve = False
                 self.need_to_redraw = True
             else:
                 self.game.m_gst.switch_state("overworld")
-        self.time = time
-
-        self.redraw(time, frame_time)
-        return False
 
     def get_evo_data(self):
         self.small_splash = self.evo.sprite
@@ -54,37 +60,46 @@ class GameStateMenuEvolve(BaseGameState):
     def redraw(self, time, frame_time):
         if self.need_to_redraw:
             self.game.r_int.new_canvas()
+            self.game.r_int.draw_rectangle(
+                (0, 0),
+                to=(1, 1),
+                col="black",
+            )
             self.draw_interface(time, frame_time)
             self.need_to_redraw = False
 
     def event_keypress(self, key, modifiers):
         if key == "interact":
-            self.evolve = True
-            self.evo.evolve(self.evo_target)
-            self.need_to_redraw = True
+            if self.evolving:
+                self.evolve = True
+                self.evo.evolve(self.evo_target)
+                self.need_to_redraw = True
+            else:
+                self.get_evolutions()
         if key == "backspace":
             self.evolving = False
             self.need_to_redraw = True
             self.game.r_int.fade = True
 
     def draw_interface(self, time, frame_time):
-        if len(self.to_evolve) > 0:
+        if self.evolving:
             self.game.r_int.draw_image(
                 self.spr_statusbox, (0.0035, 0.9),
             )
             if not self.evolve:
                 self.game.r_int.draw_image(
-                    self.big_splash, (0.5, 0.5), centre=True
+                    self.small_splash, (0.5, 0.5), centre=True
                 )
                 self.game.r_int.draw_text(
                     f"{self.evo.name} is evolving!", (0.01, 0.91), to=(0.99, 0.99), bcol=None
                 )
             else:
                 self.game.r_int.draw_image(
-                    self.small_splash, (0.5, 0.5), centre=True
+                    self.big_splash, (0.5, 0.5), centre=True
                 )
                 self.game.r_int.draw_text(
-                    f"{self.evo.name} has evolved into {self.evo_target.name}!", (0.01, 0.91), to=(0.99, 0.99), bcol=None
+                    f"{self.evo_name} has evolved into {self.evo.name}!", (0.01, 0.91), to=(0.99, 0.99), bcol=None
                 )
+                self.evolving = False
 
 
