3950d93518045866b88d72fd1a5b71b709363be6
Jetse <jetse1796@outlook.com>
2021-08-08 20:27:36 +0200

    Damage on end_turn effects applies immediately, not the turn after

--- game/combat/effects/damageeffect.py
+++ game/combat/effects/damageeffect.py
@@ -7,9 +7,24 @@ class DamageEffect(BaseEffect):
         self.target = target
         self.abs_damage = abs_dmg
         self.rel_damage = rel_dmg
+        self.spd_before_action = 1e10
         self.spd_on_action = 1e10
+        self.spd_before_end = 1e10
+
+
+    def before_action(self):
+        damage = self.abs_damage + int(self.scene.board.get_actor(self.target).stats[0] * self.rel_damage)
+        self.scene.board.inflict_damage(self.target, damage)
+        # self.scene.board.no_skip(f"{self.scene.board.get_actor(self.target).name} took {damage} damage!", particle="")
+        return True, False, False
 
     def on_action(self):
+        damage = self.abs_damage + int(self.scene.board.get_actor(self.target).stats[0] * self.rel_damage)
+        self.scene.board.inflict_damage(self.target, damage)
+        # self.scene.board.no_skip(f"{self.scene.board.get_actor(self.target).name} took {damage} damage!", particle="")
+        return True, False, False
+
+    def before_end(self):
         damage = self.abs_damage + int(self.scene.board.get_actor(self.target).stats[0] * self.rel_damage)
         self.scene.board.inflict_damage(self.target, damage)
         # self.scene.board.no_skip(f"{self.scene.board.get_actor(self.target).name} took {damage} damage!", particle="")
